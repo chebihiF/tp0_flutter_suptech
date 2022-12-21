70b9ce14ae2c1ff1ce5325fafae6be673ebd0f41
chebihiF <chebihi.f@ucd.ac.ma>
2022-12-21 11:20:48 +0100

    add list of buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color ?',
+      'What\'s your favorite animal ?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is my default Text !!!!"),
+        body: Column(children: [
+          Text('The questions'),
+          ElevatedButton(onPressed: null, child: Text('Answer 1')),
+          ElevatedButton(onPressed: null, child: Text('Answer 2')),
+          ElevatedButton(onPressed: null, child: Text('Answer 3')),
+        ]),
       ),
     );
   }
