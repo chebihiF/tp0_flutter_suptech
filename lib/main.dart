1d6106cccb85664fafc5997612a647f4241afa7a
chebihiF <chebihi.f@ucd.ac.ma>
2023-02-23 12:21:34 +0100

    update 23/02/2023 12H

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tp0/answer.dart';
-import './question.dart';
+import 'quiz.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,42 +13,44 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State {
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color ?',
+      'answers': ['Black', 'Red', 'Blue', 'Green'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal ?',
+      "answers": ['Rabiit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite instructor ?',
+      "answers": ['Mr.xxx', 'Mr.yyy', 'Mr.zzz', 'Mr.ttt'],
+    }
+  ];
+
   var _questionIndex = 0;
+
   void _answerQuestion() {
-    setState(() {
-      _questionIndex++;
-    });
+    if (_questionIndex < _questions.length) {
+      setState(() {
+        _questionIndex++;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color ?',
-        'answers': ['Black', 'Red', 'Blue', 'Green'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal ?',
-        "answers": ['Rabiit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite instructor ?',
-        "answers": ['Mr.xxx', 'Mr.yyy', 'Mr.zzz', 'Mr.ttt'],
-      }
-    ];
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-        ),
-        body: Column(children: [
-          Question(questions[_questionIndex]['questionText']),
-          ...(questions[_questionIndex]['answers'] as List<String>)
-              .map((answer) => Answer(_answerQuestion, answer))
-              .toList()
-        ]),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
       ),
-    );
+      body: _questionIndex < _questions.length
+          ? Quiz(_questions, _questionIndex, _answerQuestion)
+          : const Center(
+              child: Text('No more questions'),
+            ),
+    ));
   }
 }
